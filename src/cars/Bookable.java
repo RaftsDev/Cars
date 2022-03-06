f75e726d0a17869ef83e88ee51b1e921ba8c4a0c
Ralph <raftsdev@gmail.com>
2022-03-06 12:42:32 -0500

    Fix: Fix typos.

--- src/cars/Bookable.java
+++ src/cars/Bookable.java
@@ -15,16 +15,16 @@ public class Bookable {
     public String getName() {
         return name;
     }
-    .
+
     public void addCar(Cars car) {
         if(this.carsAvailable.contains(car)) {
-            System.out.println("The car "+this.model+" model of "+ car.getCompany+ "company  in "+ this.pickUpLocation+" at "+this.pickupDate+" already exists in the database.");
+            System.out.println("The car "+car.getModel()+" model of "+ car.getCompanyName()+ "company  in "+ car.getPickUpLocation()+" at "+car.getPickUpDate()+" already exists in the database.");
         }
         else {
             this.carsAvailable.add(car);
         }
         System.out.println("You have added car: "+ car.getCompanyName() + "--"+car.getModel()+"-("+car.getSize()+")-Location: "+car.getPickUpLocation()+" pick up time:"+car.getPickUpDate()+" in the database.");
-        System.out.println("The number of available cars is " + this.airlinesAvailable.size() +" in the database now.");
+        System.out.println("The number of available cars is " + this.carsAvailable.size() +" in the database now.");
 
     }
 
@@ -34,7 +34,7 @@ public class Bookable {
         } else {
             carsAvailable.remove(car);
             System.out.println("You have removed car "+ car.getCompanyName() + "--"+car.getModel()+"-("+car.getSize()+")-Location: "+car.getPickUpLocation()+" pick up time: "+car.getPickUpDate()+ " from the database.");
-            System.out.printf("The number of available cars is " + carsAvailable.size() +" in the database now.");
+            System.out.println("The number of available cars is " + carsAvailable.size() +" in the database now.");
         }
     }
 
